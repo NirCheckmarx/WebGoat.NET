c9f84b5e7c057f3d13fd4a2af14ff7722c1669fc
nirw <nir.weinberg@gmail.com>
2022-12-21 21:43:13 +1100

    attempt fixing the build

--- WebGoat/Content/XPathInjection.aspx.cs
+++ WebGoat/Content/XPathInjection.aspx.cs
@@ -34,11 +34,13 @@ namespace OWASP.WebGoat.NET
             string safeExpr = "//salesperson[state=$state]/text()";
             XPathExpression xpath = XPathExpression.Compile(safeExpr);
 
-            // Define variables and resolver
-		    //Implement CustomContext as a subclass of XsltContext
-		    CustomContext ctxParameters = new CustomContext(); 
-		    ctxParameters.AddVariable("state", state);		
-		    xpath.SetContext(ctxParameters);
+            XsltArgumentList varList = new XsltArgumentList();
+            varList.AddParam("userName", string.Empty, state);
+
+            // CustomContext is an application specific class, that looks up variables in the
+            // expression from the varList.
+            CustomContext context = new CustomContext(new NameTable(), varList);
+            xpath.SetContext(context);
             
             XmlNodeList list = xDoc.SelectNodes(safeExpr);
 
@@ -161,5 +163,193 @@ namespace OWASP.WebGoat.NET
             }
         }
     }
+    // The interface that resolves and executes a specified user-defined function.
+public class XPathExtensionFunctions : System.Xml.Xsl.IXsltContextFunction
+{
+    // The data types of the arguments passed to XPath extension function.
+    private System.Xml.XPath.XPathResultType[] argTypes;
+    // The minimum number of arguments that can be passed to function.
+    private int minArgs;
+    // The maximum number of arguments that can be passed to function.
+    private int maxArgs;
+    // The data type returned by extension function.
+    private System.Xml.XPath.XPathResultType returnType;
+    // The name of the extension function.
+    private string FunctionName;
+
+    // Constructor used in the ResolveFunction method of the custom XsltContext
+    // class to return an instance of IXsltContextFunction at run time.
+    public XPathExtensionFunctions(int minArgs, int maxArgs,
+        XPathResultType returnType, XPathResultType[] argTypes, string functionName)
+    {
+        this.minArgs = minArgs;
+        this.maxArgs = maxArgs;
+        this.returnType = returnType;
+        this.argTypes = argTypes;
+        this.FunctionName = functionName;
+    }
+
+    // Readonly property methods to access private fields.
+    public System.Xml.XPath.XPathResultType[] ArgTypes
+    {
+        get
+        {
+            return argTypes;
+        }
+    }
+    public int Maxargs
+    {
+        get
+        {
+            return maxArgs;
+        }
+    }
+
+    public int Minargs
+    {
+        get
+        {
+            return maxArgs;
+        }
+    }
+
+    public System.Xml.XPath.XPathResultType ReturnType
+    {
+        get
+        {
+            return returnType;
+        }
+    }
+
+    // XPath extension functions.
+
+    private int CountChar(XPathNodeIterator node, char charToCount)
+    {
+        int charCount = 0;
+        for (int charIdx = 0; charIdx < node.Current.Value.Length; charIdx++)
+        {
+            if (node.Current.Value[charIdx] ==  charToCount)
+            {
+                charCount++;
+            }
+        }
+        return charCount;
+    }
+
+    // This overload will not force the user
+    // to cast to string in the xpath expression
+    private string FindTaskBy(XPathNodeIterator node, string text)
+    {
+        if (node.Current.Value.Contains(text))
+            return node.Current.Value;
+        else
+            return "";
+    }
+
+    private string Left(string str, int length)
+    {
+        return str.Substring(0, length);
+    }
+
+    private string Right(string str, int length)
+    {
+        return str.Substring((str.Length - length), length);
+    }
+
+    // Function to execute a specified user-defined XPath extension
+    // function at run time.
+    public object Invoke(System.Xml.Xsl.XsltContext xsltContext,
+                   object[] args, System.Xml.XPath.XPathNavigator docContext)
+    {
+        if (FunctionName == "CountChar")
+            return (Object)CountChar((XPathNodeIterator)args[0],
+                                             Convert.ToChar(args[1]));
+        if (FunctionName == "FindTaskBy")
+            return FindTaskBy((XPathNodeIterator)args[0],
+                                          Convert.ToString(args[1]));
+
+        if (FunctionName == "Left")
+            return (Object)Left(Convert.ToString(args[0]),
+                                             Convert.ToInt16(args[1]));
+
+        if (FunctionName == "Right")
+            return (Object)Right(Convert.ToString(args[0]),
+                                             Convert.ToInt16(args[1]));
+
+        return null;
+    }
+}
+using System;
+using System.Xml;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+using System.IO;
+
+namespace XPathExtensionFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char keychar = ' ';
+
+            while (true)
+            {
+                Console.Write("\r\nEnter key character to count in names: ");
+                keychar = Console.ReadKey().KeyChar;
+                if (keychar.Equals('q')) return;
+
+                try
+                {
+                    // Load source XML into an XPathDocument object instance.
+                    XPathDocument xmldoc = new XPathDocument("Tasks.xml");
+
+                    // Create an XPathNavigator from the XPathDocument.
+                    XPathNavigator nav = xmldoc.CreateNavigator();
+
+                    //Create argument list and add the parameters.
+                    XsltArgumentList varList = new XsltArgumentList();
+
+                    varList.AddParam("charToCount", string.Empty, keychar);
+
+                    // Create an instance of custom XsltContext object.
+                    // Pass in the XsltArgumentList object
+                    // in which the user-defined variable will be defined.
+                    CustomContext context = new CustomContext(new NameTable(), varList);
+
+                    // Add a namespace definition for the namespace prefix that qualifies the
+                    // user-defined function name in the query expression.
+                    context.AddNamespace("Extensions", "http://xpathExtensions");
+
+                    // Create the XPath expression using extension function to select nodes
+                    // that contain 2 occurences of the character entered by user.
+                    XPathExpression xpath = XPathExpression.Compile(
+                        "/Tasks/Name[Extensions:CountChar(., $charToCount) = 2]");
+
+                    xpath.SetContext(context);
+                    XPathNodeIterator iter = nav.Select(xpath);
+
+                    if (iter.Count.Equals(0))
+                        Console.WriteLine("\n\n\rNo results contain 2 instances of "
+                                                                 + keychar.ToString());
+                    else
+                    {
+                        Console.WriteLine("\n\n\rResults that contain 2 instances of : "
+                                                                 + keychar.ToString());
+                        // Iterate over the selected nodes and output the
+                        // results filtered by extension function.
+                        while (iter.MoveNext())
+                        {
+                            Console.WriteLine(iter.Current.Value);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
 }
 
