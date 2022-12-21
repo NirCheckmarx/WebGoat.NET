1badc89ec773baa0e69fc69aae0b6be074f0f830
nirw <nir.weinberg@gmail.com>
2022-12-21 21:04:48 +1100

    testing fix for XPathInjection

--- WebGoat/Content/XPathInjection.aspx.cs
+++ WebGoat/Content/XPathInjection.aspx.cs
@@ -25,9 +25,9 @@ namespace OWASP.WebGoat.NET
         {
             XmlDocument xDoc = new XmlDocument();
             xDoc.LoadXml(xml);
-            XmlNodeList list = xDoc.SelectNodes("//salesperson[state='" + state + "']");
+            // XmlNodeList list = xDoc.SelectNodes("//salesperson[state='" + state + "']");
             
-            /*string expr = "//salesperson[state='" + state + "']";
+            // string expr = "//salesperson[state='" + state + "']";
             //XmlNodeList list = xDoc.SelectNodes(expr);
 
             string safeExpr = "//salesperson[state=$state]/text()";
@@ -39,7 +39,7 @@ namespace OWASP.WebGoat.NET
 		    ctxParameters.AddVariable("state", state);		
 		    xpath.SetContext(ctxParameters);
             
-            XmlNodeList list = xDoc.SelectNodes(safeExpr);*/
+            XmlNodeList list = xDoc.SelectNodes(safeExpr);
 
             if (list.Count > 0)
             {
