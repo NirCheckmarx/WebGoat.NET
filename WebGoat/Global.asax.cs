98be1646140135c4df4a1ce0da3d45ea17ef788b
nirw <nir.weinberg@gmail.com>
2022-12-22 22:46:21 +1100

    reverting the xframe attempted fix which didn't work and attemting to fix CSP issues

--- WebGoat/Global.asax.cs
+++ WebGoat/Global.asax.cs
@@ -29,7 +29,7 @@ namespace OWASP.WebGoat.NET
 
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
-            //HttpContext.Current.Response.AddHeader("X-Frame-Options", "DENY");
+            HttpContext.Current.Response.AddHeader("X-Frame-Options", "DENY");
         }
 
         void Application_PreSendRequestHeaders(Object sender, EventArgs e)
