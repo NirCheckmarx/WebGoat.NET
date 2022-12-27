a284687cd650892dd0d257da683ce3938b98cc12
nirw <nir.weinberg@gmail.com>
2022-12-27 12:36:21 +1100

    attempt fixing the build

--- WebGoat/WebGoatCoins/Autocomplete.ashx.cs
+++ WebGoat/WebGoatCoins/Autocomplete.ashx.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Net.WebUtility;
+using System.Text.RegularExpressions;
 using System.Data;
 using OWASP.WebGoat.NET.App_Code;
 using OWASP.WebGoat.NET.App_Code.DB;
