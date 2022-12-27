899cef0770c0a2c1430d48342110dcbd18b623c8
nirw <nir.weinberg@gmail.com>
2022-12-27 12:41:51 +1100

    attempt fixing the build using a simple / framework solution for encoding unsafe HTML

--- WebGoat/WebGoatCoins/Autocomplete.ashx.cs
+++ WebGoat/WebGoatCoins/Autocomplete.ashx.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Net.WebUtility;
-using System.Text.RegularExpressions;
 using System.Data;
 using OWASP.WebGoat.NET.App_Code;
 using OWASP.WebGoat.NET.App_Code.DB;
@@ -52,17 +50,8 @@ namespace OWASP.WebGoat.NET.WebGoatCoins
         }
         private static string SanitizeJson(string UnsafeJsonString)
         {
-            const string tagWhiteSpace = @"(>|$)(\W|\n|\r)+<";
-            const string stripFormatting = @"<[^>]*(>|$)";
-            const string lineBreak = @"<(br|BR)\s{0,1}\/{0,1}>";
-            var lineBreakRegex = new Regex(lineBreak, RegexOptions.Multiline);
-            var stripFormattingRegex = new Regex(stripFormatting, RegexOptions.Multiline);
-            var tagWhiteSpaceRegex = new Regex(tagWhiteSpace, RegexOptions.Multiline);
-            var safeJsonString = UnsafeJsonString;
-            safeJsonString = System.Net.WebUtility.HtmlDecode(safeJsonString);
-            safeJsonString = tagWhiteSpaceRegex.Replace(safeJsonString, "><");
-            safeJsonString = lineBreakRegex.Replace(safeJsonString, Environment.NewLine);
-            safeJsonString = stripFormattingRegex.Replace(safeJsonString, string.Empty);
+           
+            var safeJsonString = HttpUtility.HtmlEncode(UnsafeJsonString);
             return safeJsonString;
 
         }
